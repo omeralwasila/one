895563b222efd492fce5f4a2a4a2eba8a3fc1ec7
Omer Elawsila <omeralwaseela97@gmail.com>
2021-08-18 14:36:14 -0700

    new

---
+++ src/component/button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Photo from '../img/1.jpg';
+
+
+
+const Button =()=>{
+    const show = () => {
+      
+      console.log('hi')
+
+
+
+       }
+
+    return <button className="btn btn-primary button" type="button" onClick={show}>click me</button>
+
+
+}
+ export default Button;
---
+++ src/component/photo.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './photo.css';
+import photo from '../img/1.jpg';
+
+
+const Photo =()=>{
+
+
+    return (
+        <div>
+          <img src={photo} alt="MyImage"/>
+        </div>
+      );
+
+}
+ export default Photo;
